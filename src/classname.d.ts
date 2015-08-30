12974a7d4d65920b46d80f888cb51fb9015dde79
Max Koverdyaev <shadeglare@gmail.com>
2015-08-30 20:00:28 +0300

    refactored

--- src/classname.d.ts
+++
@@ -1,7 +0,0 @@
-declare module classname {
-    interface ClassNameFragment {
-        [key: string]: boolean;
-    }
-    function format(...args: Array<string | number | ClassNameFragment>): string;
-    function none(): void;
-}
--- src/classname.ts
+++ src/classname.ts
@@ -1,30 +1,22 @@
-module classname {
+export interface ClassNameFragment {
+    [key: string]: boolean;
+}
 
-    export interface ClassNameFragment {
-        [key: string]: boolean;
-    }
-
-    export function format(...args: Array<string | number | ClassNameFragment>): string {
-        return args.reduce<Array<string | number>>((accum, arg) => {
-            do {
-                if (!arg) {
-                    break;
-                } else if (typeof arg === "string" || typeof arg === "number") {
-                    accum.push(arg);
-                } else {
-                    for (var key in arg) {
-                        if (arg[key]) {
-                            accum.push(key);
-                        }
+export function format(...args: Array<string | number | ClassNameFragment>): string {
+    return args.reduce<Array<string | number>>((accum, arg) => {
+        do {
+            if (!arg) {
+                break;
+            } else if (typeof arg === "string" || typeof arg === "number") {
+                accum.push(arg);
+            } else {
+                for (var key in arg) {
+                    if (arg[key]) {
+                        accum.push(key);
                     }
                 }
-            } while (false);
-            return accum;
-        }, []).join(" ");
-    }
-
-    export function none() {
-        throw new Error("Not implemented yet");
-    }
-
+            }
+        } while (false);
+        return accum;
+    }, []).join(" ");
 }

7452119568e5e4702c2d0e459db986f05e9d5fc6
abeall <abeall@decisionlens.com>
2017-09-07 18:21:41 -0400

    Added null | undefined to ClassNameObject value

--- src/classnamer.ts
+++ src/classnamer.ts
@@ -1,17 +1,16 @@
 export type ClassNamePrimitive = string | number | boolean;
-export type ClassNameObject = { [key: string]: boolean };
+export type ClassNameObject = { [key: string]: boolean | null | undefined };
 export type ClassNameFragment = ClassNamePrimitive | ClassNameObject | ClassNameFragmentList | null | undefined;
 export interface ClassNameFragmentList extends Array<ClassNameFragment> { }
 
 export default function classnamer(...args: ClassNameFragment[]) {
     let accum = "";
-    for (let i = 0; i < arguments.length; i++) {
-        let arg = arguments[i];
+    for (let i = 0; i < args.length; i++) {
+        let arg = args[i];
         if (!arg) {
             continue;
         }
-        let argType = typeof arg;
-        if (argType === "string" || argType === "number" || argType === "boolean") {
+        if (typeof arg === "string" || typeof arg === "number" || typeof arg === "boolean") {
             accum += " " + arg;
         } else if (Array.isArray(arg)) {
             accum += " " + classnamer.apply(null, arg);
